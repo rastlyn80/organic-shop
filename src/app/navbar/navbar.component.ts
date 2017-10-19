9efc4310689e6a8ed46da077a1cc97ce228d1700
Gabriele Baldocci <gabrielebaldocci@gabrielebaldocci.com>
2017-10-19 21:38:49 +0100

    Created auth service and refactored navbar and login components

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,7 +1,5 @@
-import { AngularFireAuth } from 'angularfire2/auth';
+import { AuthService } from './../auth.service';
 import { Component, OnInit } from '@angular/core';
-import * as firebase from 'firebase';
-import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'navbar',
@@ -9,14 +7,12 @@ import { Observable } from 'rxjs/Observable';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent  {
-  user$: Observable<firebase.User>;
 
-  constructor(private afAuth: AngularFireAuth) { 
-    this.user$ = afAuth.authState;
-  }
+  constructor(public auth: AuthService) { 
 
-  logout() {
-    this.afAuth.auth.signOut();
-  }
+    }
 
+    logout() {
+      this.auth.logout();
+    }
 }
